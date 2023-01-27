81fe233f1366b66e3b913347dd63fab774d5fd2c
Martin <martinllara@gmail.com>
2023-01-27 13:31:13 -0300

    mandando questoes python

---
+++ sprint_3/Exercicios_py/python4.py
@@ -0,0 +1,4 @@
+#Escreva um código Python que imprime todos os números primos de 0 até 100.
+final = 101
+for i in range(2,final):
+    pass
--- sprint_3/Exercicios_py/python8.py
+++ sprint_3/Exercicios_py/python8.py
@@ -4,6 +4,6 @@ Obs: Palíndromo é uma palavra que permanece igual se lida de traz pra frente."
 palavras = ['maça', 'arara', 'audio', 'radio', 'radar', 'moto']
 for i in palavras:
     if i == i[::-1]:
-        print(f'{i} é um palíndromo')
+        print(f'A palavra: {i} é um palíndromo')
     else:
-        print(f'{i} não é um palíndromo')
+        print(f'A palavra: {i} não é um palíndromo')
